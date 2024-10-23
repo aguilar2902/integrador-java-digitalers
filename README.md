d47d6c6d2c1cd758a5f06cd9c54c1fe28510aac7
Jonathan <aguilarjonathan2902@gmail.com>
2024-10-23 14:04:37 -0300

    Se registran las configuraciones iniciales del proyecto Spring Boot

---
+++ README.md
@@ -0,0 +1,2 @@
+# integrador-java-digitalers
+En esta parte del integrador, realizo un aplicacion web con Spring Boot, implementando una API utilizando el servicio REST
---
+++ src/main/java/com/educacionit/aguilar_backend/AguilarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.educacionit.aguilar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AguilarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AguilarBackendApplication.class, args);
+    }
+
+}
